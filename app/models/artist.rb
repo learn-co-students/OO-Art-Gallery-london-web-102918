d3eece481fabf660b63a9c6c8f41dd33d3297904
github username <github email address>
2018-11-07 21:15:05 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,35 @@
 class Artist
 
   attr_reader :name
+  attr_accessor :years_active
+
+  @@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings_by
+    Painting.all.select {|p| p.artist.name == self.name}
+  end
+
+  def galleries_list
+    paintings_by.map {|p| p.gallery.gallery}
+  end
+
+  def cities_list
+    paintings_by.map {|p| p.gallery.city}.uniq
+  end
+
+  def self.average_years
+    total = self.all.map {|a| a.years_active}
+    total.inject(0){|sum, x| sum += x}.to_f / total.length
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,27 @@ def reload
   load 'config/environment.rb'
 end
 
+#Artists
+raphael = Artist.new("Raphael", 20)
+donatello = Artist.new("Donatello", 18)
+michelangelo = Artist.new("Michelangelo", 50)
+leonardo = Artist.new("Leonardo", 34)
+
+#Galleries
+louvre = Gallery.new("Louvre", "Paris")
+tate = Gallery.new("Tate", "London")
+moma = Gallery.new("Moma", "NY")
+rijks= Gallery.new("Rijks", "Amsterdam")
+saatchi = Gallery.new("Saatchi", "London")
+
+#Paintings
+mona = Painting.new("Mona", "old", leonardo, louvre)
+dog = Painting.new("Dog", "new", donatello, moma)
+house = Painting.new("House", "old", michelangelo, tate)
+cat = Painting.new("Cat", "modern", raphael, rijks)
+lighthouse = Painting.new("Lighthouse", "baroque", leonardo, louvre)
+elephant = Painting.new("Elephant", "modern", raphael, tate)
+rabbit = Painting.new("Rabbit in Headlights", "art nouveau", donatello, saatchi)
+
 binding.pry
+"Awesome"
