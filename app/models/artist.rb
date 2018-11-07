10c1a41191905ef5fcdc572c95085d2133d1310a
github username <github email address>
2018-11-07 15:35:53 +0000

    I'm not sure

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,37 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+  @@all =[]
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def list_all_paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def self.average_years
+    arr = @@all.map do |years| years.years_active
+    end
+    arr.inject(:+)/arr.length
+  end
+
+  def galleries
+    self.list_all_paintings.map do |painting| painting.gallery.name
+    end.uniq
+  end
+
+  def cities
+    self.list_all_paintings.map do |painting| painting.gallery.city
+    end.uniq
   end
 
 
