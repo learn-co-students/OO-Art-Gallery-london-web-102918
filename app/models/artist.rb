3ffdcb6b70069f8ba0ac74d110f47c06add42cf3
github username <github email address>
2018-11-08 15:32:25 +0000

    done

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,11 +1,38 @@
 class Artist
 
-  attr_reader :name
+  attr_reader :name, :years_active
+
+@@all = []
 
   def initialize(name, years_active)
     @name = name
     @years_active = years_active
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 
+#Get a list of all the paintings by a specific artists
+
+    def paintings
+      Painting.all.select {|p| p.artist == self}
+    end
+#Get a list of all the galleries that a specific artist has paintings in
+    def galleries
+      paintings.map {|p| p.gallery}
+    end
+#Get a list of all cities that contain galleries that a specific artist has paintings in
+      def cities
+        galleries.map {|g| g.city}
+      end
+
+#Find the average years of experience of all artists
+      def self.avr_years
+        sum = 0
+        Artist.all.each {|a| sum += a.years_active }
+        sum.to_f/Artist.all.length
+      end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,24 @@
 class Painting
 
-  attr_reader :title, :style
+  attr_reader :title, :style, :artist, :gallery
+   @@all = []
 
-  def initialize(title, style)
+  def initialize(title, style, artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #Get a list of all painting styles (a style should not appear more than once in the list)
+
+  def self.style
+    @@all.map {|p| p.style}.uniq
   end
 
 end
