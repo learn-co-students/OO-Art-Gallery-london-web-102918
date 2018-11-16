22e8b5de8ff97cd9f7db83775bfce4b669402e47
github username <github email address>
2018-11-16 09:11:11 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,26 @@
 class Gallery
 
-  attr_reader :name, :city
-
-def initialize(name, city)
-  @name = name
+  attr_reader :gallery, :city
+@@all =[]
+def initialize(gallery, city)
+  @name = gallery
   @city = city
+  @@all << self
 end
+def self.all
+  @@all
+end
+def self.cities_with_gallery
+    Gallery.all.map{|g| g.city}.uniq
+  end
+
+  def artists_at_gallery
+    Painting.all.select{|p| p.gallery == self}.map{|p| p.artist}.uniq
+  end
+
+  def years_of_experience_at_gallery
+    artists_at_gallery.map{|a| a.years_active}.reduce(){|sum, years| sum+=years}
+  end
 
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,10 +1,19 @@
 class Painting
 
   attr_reader :title, :style
-
-  def initialize(title, style)
+@@all=[]
+  def initialize(title, style,artist, gallery)
     @title = title
     @style = style
+    @artist = artist
+    @gallery =gallery
+    @@all<< self
   end
+  def self.all
+    @@all
+  end
+  def self.styles
+      Painting.all.map{|p| p.style}.uniq
+    end
 
 end
